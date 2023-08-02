92e0471798c29a0917b2cdebcca28961dd3e413c
Ernesto <48799528+ernestod10@users.noreply.github.com>
2023-08-02 15:41:09 -0400

    Update mart.js

--- routes/mart.js
+++ routes/mart.js
@@ -5,17 +5,17 @@ module.exports = (pool) => {
   router.post('/', async (req, res) => {
     const imageData = req.body; // Assuming the entire JSON data is sent in the request body
     
-    try {
+  
 
       const query = 'INSERT INTO "test" (test1) VALUES ($1)';
       const values = [JSON.stringify(imageData)]; // Serialize the entire JSON data
-      await pool.query(query, values);
+     // await pool.query(query, values);
 
-      res.status(200).send('Data received and inserted successfully');
-    } catch (error) {
-      console.error('Error inserting data:', error);
-      res.status(500).send('An error occurred while inserting data:',error);
-    }
+
+
+      console.log(values);
+    
+  
   });
 
 return router;
