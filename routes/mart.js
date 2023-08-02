7e30d7dc580d4860512436a1fe56c7cd1b682df7
Ernesto <48799528+ernestod10@users.noreply.github.com>
2023-08-02 15:43:08 -0400

    Update mart.js

--- routes/mart.js
+++ routes/mart.js
@@ -5,17 +5,17 @@ module.exports = (pool) => {
   router.post('/', async (req, res) => {
     const imageData = req.body; // Assuming the entire JSON data is sent in the request body
     
-  
+
 
       const query = 'INSERT INTO "test" (test1) VALUES ($1)';
       const values = [JSON.stringify(imageData)]; // Serialize the entire JSON data
-     // await pool.query(query, values);
+      //await pool.query(query, values);
 
-
-
-      console.log(values);
+      res.status(200).send(values);
     
-  
+      console.log(values) ;
+
+
   });
 
 return router;
