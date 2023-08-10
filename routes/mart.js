94122a96e5c95ba7e3ec11c656edf5a4b671d589
Ernesto <48799528+ernestod10@users.noreply.github.com>
2023-08-10 15:17:12 -0400

    Update mart.js

--- routes/mart.js
+++ routes/mart.js
@@ -52,6 +52,83 @@ module.exports = (pool) => {
     console.log(values);
 });
 
+
+router.get('/', async (req, res) => {
+  try {
+      const query = 'SELECT * FROM "apitest"';
+      const result = await pool.query(query);
+      res.status(200).json(result.rows);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+
+  router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+
+    const {
+        idEmpresa,
+        title,
+        type,
+        url,
+        message,
+        owner,
+        size,
+        created,
+        modified,
+        available,
+        error,
+        __typename,
+        menu
+    } = updatedData;
+
+    const query = `UPDATE "apitest" SET 
+                  idEmpresa = $1,
+                  title = $2,
+                  type = $3,
+                  url = $4,
+                  message = $5,
+                  owner = $6,
+                  size = $7,
+                  created = $8,
+                  modified = $9,
+                  available = $10,
+                  error = $11,
+                  __typename = $12,
+                  menu = $13
+                  WHERE id = $14`;
+
+    const values = [
+        idEmpresa,
+        title,
+        type,
+        url,
+        message,
+        owner,
+        size,
+        created,
+        modified,
+        available,
+        error,
+        __typename,
+        JSON.stringify(menu),
+        id
+    ];
+
+    try {
+        await pool.query(query, values);
+        res.status(200).send('Record updated successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+  });
+
+
+
   router.post('/img/', async (req, res) => {
     //await pool.query(query, values);
     res.status(200).json({ url: 'https://dummyimage.com/600x400/000/fff.jpg&text=test' });
